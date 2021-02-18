ba9037ea8d48ee151361fa89971e923496ca6036
Thomas Geissl <thomas.geissl@gmail.com>
2021-02-18 17:20:04 +0100

    cleaned up

--- arduino/standalone/standalone.ino
+++ arduino/standalone/standalone.ino
@@ -1,8 +1,6 @@
 #include "./defines.h"
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-#include <PacketSerial.h>
-
 #include "./startAnimation.h"
 
 #ifdef __AVR__
@@ -11,8 +9,6 @@
 
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel (TOTAL_NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-PacketSerial packetSerial;
-bool dirty;
 
 void setup() {
   strip.begin();
@@ -20,28 +16,10 @@ void setup() {
   strip.clear();
   strip.show();
 
-//  packetSerial.begin(115200);
-//  packetSerial.setPacketHandler(&onPacketReceived);
-
   create_startAnimation();
+  startAnimation.trigger(strip);
+
 }
 
 void loop() {
-  startAnimation.trigger(strip);
-
-//  packetSerial.update();
-//
-//  if (packetSerial.overflow())
-//  {
-//  }
-//  if (dirty) {
-//    strip.show();
-//    dirty = false;
-//  }
-}
-
-void onPacketReceived(const uint8_t* buffer, size_t size)
-{
-  strip.setPixelColor(buffer[0] + LED_OFFSET, buffer[1], buffer[2], buffer[3]);
-  dirty = true;
 }
